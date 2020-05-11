7240b025243a7dc6cc4493fb64083f1a87b666a2
jazmyn cortez <jazmyncortez@gmail.com>
2020-05-10 22:44:24 -0700

    completed to the best I know

--- code-challenges.js
+++ code-challenges.js
@@ -4,60 +4,110 @@
 // Use the test variables provided.
 
 var num1 = 15
-// Expected output: "15 is divisible by three"
+// // Expected output: "15 is divisible by three"
 
 var num2 = 0
 // Expected output: "0 is divisible by three"
 
 var num3 = -7
 // Expected output: "-7 is not divisble by three"
-
-
-
-
+//create a function that will use modulo, first if will check to see if its divisible by 3 and return the number is divisible by three
+const divisibleChecker = (num) => {
+  if (num % 3 === 0){
+    return `${num} is divisible by 3`
+  }
+  //else if will show what will be returned if the number is not divisible by 3
+    else if (num % 3 !== 0) {
+    return `${num} is not divisiable by 3`
+//catch all statement that will show if the code works or not
+  } else {
+    return "broken"
+  }
+}
+//call the code using 3 different console logs
+console.log(divisibleChecker(15));
+console.log(divisibleChecker(0))
+console.log(divisibleChecker(-7))
 
 // --------------------2) Create a function that takes in the randomNouns variable and returns an array with all the words capitalized.
 
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
-
-
-
+//I use the .map method
+//use the .toUpperCase method too
+//console log it
+upperCaseLetters = (randomNouns)
+  for (var i = 0; i < randomNouns.length; i++) {
+    randomNouns[i] = randomNouns.map(i => i.toUpperCase)
+ // return (randomNouns.toUpperCase)
+}
+console.log(randomNouns)
 
 
 // --------------------3) Create a function that takes in the mixedDataArray array and returns an array with ONLY NUMBERS sorted from least to greatest.
 
 var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
-// Expected output: [-8, 0, 8, 46, 59, 90, 107]
+// // Expected output: [-8, 0, 8, 46, 59, 90, 107]
+//
+const noWords = (value) => {
+  let words = [null, undefined, true, false, ""]
+  let array = words.split()
+  return array.filer(value => words.indexOf(value) === -1).join()
+}
+console.log(noWords(mixedDataArray))
 
-
-
-
-
-// --------------------4) Create a function that takes in a string and logs the index of the first vowel.
-
+// // --------------------4) Create a function that takes in a string and logs the index of the first vowel.
+//
 var vowelTester1 = "learn"
-// Expected output: 1
-var vowelTester2 = "throw"
-// Expected output: 3
-
-
+// // Expected output: 1
+// var vowelTester2 = "throw"
+// // Expected output: 3
+//create a for loop
+//use indexOf method
+//input what my vowels are
+//
+const firstVowel = (string) => {
+let vowels = {"a", "e", "i", "o", "u"}
+return(string.indexOf(vowels))
+}
+console.log(firstVowel(vowelTester1))
 
 
 
 
 // --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
+//im going to use a for loop
+//im guessing I can type in two num as variables and symbol within the argument
+//I will take my code I have already written for the modulo question and modify it here
+const divisibleChecker = (num1, symbol, num2) => {
+  if (num1 % num2 === 0){
+    return `${num1} is divisible by ${num2}`
+  }
+  //else if will show what will be returned if the number is not divisible by 3
+    else if (num1 % num2 !== 0) {
+    return `${num1} is not divisiable by ${num2}`
+//catch all statement that will show if the code works or not
+    } else if (num1 * num2) {
+      return `${num1} * ${num2}`
+    }
+      else if (num1 - num2){
+      return `${num1} - ${num2}`
 
+    } else if (num1 / num2){
+      return `${num1} / ${num2}`
 
+    } else {
+      return "Can't devide by 0"
+}
 
 
 
 // Uncomment and use the following console logs to test your function
-// console.log(calculator(3, "*", 9))
+console.log(calculator(3, "*", 9))
 // Expected output: 27
 
-// console.log(calculator(16, "+", 3))
+console.log(calculator(16, "+", 3))
 // Expected output: 19
 
-// console.log(calculator(89, "/", 0))
+console.log(calculator(89, "/", 0))
 // Expected output: "Can't divide by 0!"
--- interview-questions.md
+++ interview-questions.md
@@ -4,49 +4,50 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. Why would you use the method super()?
 
-  Your answer:
+  Your answer: I really don't know the purpose of super()
 
-  Researched answer:
+  Researched answer: super keyword can be used to call overridden methods, access hidden fields or invoke a superclass's constructor
 
 
 
 2. What is a virtual DOM?
 
-  Your answer:
+  Your answer: Document Object model - Idk what it means but I know what it stands for
 
-  Researched answer:
+  Researched answer: Document Object Model - it's an interface that takes our URL and changes the document
 
 
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to display "Hello World" in the browser?
 
-  Your answer:
+  Your answer: You need an opening of a class (class app extends component ), you need render, you need return statement, you need an h1 tag, and then you need to export default app
 
-  Researched answer:
+  Researched answer: const element = <h1>Hello, world</h1>;
+ReactDOM.render(element, document.getElementById('root'));
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: jsx is what the javascript language that is used within react
 
-  Researched answer:
+  Researched answer: JSX is an XML/HTML-like syntax used by React that extends ECMAScript so that XML/HTML-like text can co-exist with JavaScript/React code.
 
 
 
 5. What is state in React?
 
-  Your answer:
+  Your answer: a house for pieces of code
 
-  Researched answer:
+  Researched answer: React is a JavaScript library for building user interfaces.
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: when other languages are wrapped within javascript?...
 
-  Researched answer:
+  Researched answer: Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
 
 
 
@@ -54,9 +55,18 @@ Answer the following questions. First, without external resources. Challenge you
 
 Research and define the following terms to the best of your ability.
 
-- React lifecycle methods
-- API
-- event.preventDefault()
-- DOM events
-- http verbs
-- MVC
+- React lifecycle methods - Each component has several “lifecycle methods” that you can override to run code at particular times in the process. You can use this lifecycle diagram as a cheat sheet. In the list below, commonly used lifecycle methods are marked as bold. The rest of them exist for relatively rare use cases.
+
+Examples being - mounting, updating, unmounting, error handling, other APIs etc.
+
+- API - An API is basically a set of data, often in JSON format with specified endpoints. When we access data from an API, we want to access specific endpoints within that API framework.
+
+- event.preventDefault() - React uses synthetic events to handle events from button, input and form elements. A synthetic event is a shell around the native DOM event with additional information for React. ... In this case, a preventDefault is called on the event when submitting the form to prevent a browser reload/refresh.
+
+- DOM events - Your event handlers will be passed instances of SyntheticEvent , a cross-browser wrapper around the browser's native event. It has the same interface as the browser's native event, including stopPropagation() and preventDefault() , except the events work identically across all browsers.
+
+- http verbs - The Hypertext Transfer Protocol is an application protocol for distributed, collaborative, hypermedia information systems.
+
+Examples - POST, GET, PUT, PATCH, and DELETE
+
+- MVC - Model–view–controller is a software design pattern commonly used for developing user interfaces which divides the related program logic into three interconnected elements. This is done to separate internal representations of information from the ways information is presented to and accepted from the user.
